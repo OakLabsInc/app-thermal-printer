94ce730e22fb88d6ee4c77654b929bd4d88efb62
Richard Joffray <rjoffray@richards-mbp.lan>
2020-02-18 14:45:38 -0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,63 @@
+
+const oak = require('oak')
+const { join } = require('path')
+const _ = require('lodash')
+
+oak.catchErrors()
+
+const express = require('express')
+const stylus = require('stylus')
+const app = express()
+
+
+const port = process.env.PORT ? _.toNumber(process.env.PORT) : 9000
+const printer = require(join(__dirname, 'printer'))
+
+let publicPath = join(__dirname, 'public')
+let viewsPath = join(__dirname, 'views')
+
+let window = null
+
+app.set('views', viewsPath)
+app.set('view engine', 'pug')
+app.use(stylus.middleware({
+  src: viewsPath,
+  dest: publicPath
+}))
+
+app.use(express.static(publicPath))
+
+app.listen(port, function () {
+  oak.on('ready', () => loadWindow())
+})
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+
+function loadWindow () {
+
+  window = oak.load({
+    url: `http://localhost:${port}/`,
+    ontop: false,
+    insecure: true,
+    flags: ['enable-vp8-alpha-playback'],
+    size: "1080x1920",
+    sslExceptions: ['localhost'],
+    background: '#ffffff',
+    scripts: [
+      {
+        name: 'lodash',
+        path: 'lodash'
+      },
+      {
+        name: 'uuid',
+        path: 'uuid'
+      }
+    ]
+  }).on('print.examples', function () {
+    printer.printExamples()
+  })
+
+}
